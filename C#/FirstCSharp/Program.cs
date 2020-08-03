90c763d1c8b0d00dec652de9ad9fa6397118f247
caseyroy <65331620+caseyroy@users.noreply.github.com>
2020-08-03 13:20:32 -0700

    fundamentalsOne

--- C#/FirstCSharp/Program.cs
+++ C#/FirstCSharp/Program.cs
@@ -6,6 +6,29 @@ namespace FirstCSharp
     {
         static void Main(string[] args)
         {
+            // loop from 1 to 5 including 5
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            // loop from 1 to 5 excluding 5
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            int j = 1;
+            while (j < 6)
+            {
+                Console.WriteLine(j);
+                j = j + 1;
+            }
+            Random rand = new Random();
+            for (int val = 0; val < 10; val++)
+            {
+                //Prints the next random value between 2 and 8
+                Console.WriteLine(rand.Next(2, 8));
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
---
+++ C#/fundamentalsOne/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace fundamentalsOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int i = 1;
+            // while (i <= 255)
+            // {
+            //     System.Console.WriteLine(i);
+            //     i++;
+            // }
+            int j = 1;
+            while (j <= 100)
+            // for (int j = 1; j <= 100; j++)
+            {
+                if (j % 3 == 0 && j % 5 == 0)
+                {
+                    System.Console.WriteLine("FizzBuzz");
+                    System.Console.WriteLine(j);
+                }
+                else if (j % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                    System.Console.WriteLine(j);
+                }
+                else if (j % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                    System.Console.WriteLine(j);
+                }
+                j++;
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
